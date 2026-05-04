0c70287d92c353d921693525911cbbfe6f4b1dd9
Sonu9110 <sonu9110@github.com>
2026-05-04 19:03:30 +0530

    Fix supabase import in all route files

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('../db');
+const { supabase } = require('../db');
 const { authenticateUser } = require('../middleware/auth');
 const { sendOrderNotifications } = require('../utils/notify');
 
